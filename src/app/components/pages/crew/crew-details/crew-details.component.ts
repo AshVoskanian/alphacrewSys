dc6fe7bf70ec69f519d202431cda101b845b36f5
Ashot Voskanian <ashvoskanian@gmail.com>
2025-10-30 22:05:57 +0400

    crew details in progress

--- src/app/components/pages/crew/crew-details/crew-details.component.ts
+++ src/app/components/pages/crew/crew-details/crew-details.component.ts
@@ -10,10 +10,11 @@ import { TransformedSkill } from "../../../../shared/interface/schedule";
 import { CREW_SKILLS } from "../../../../shared/data/skills";
 import { TitleCasePipe } from "@angular/common";
 import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
+import { CrewProfileComponent } from "./crew-profile/crew-profile.component";
 
 @Component({
   selector: 'app-crew-details',
-  imports: [ CardComponent, TitleCasePipe, NgbNavModule ],
+  imports: [ CardComponent, TitleCasePipe, NgbNavModule, CrewProfileComponent ],
   templateUrl: './crew-details.component.html',
   styleUrl: './crew-details.component.scss'
 })
---
+++ src/app/components/pages/crew/crew-details/crew-profile/crew-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Select2Module } from "ng-select2-component";
+
+@Component({
+  selector: 'app-crew-profile',
+  imports: [ Select2Module ],
+  templateUrl: './crew-profile.component.html',
+  styleUrl: './crew-profile.component.scss'
+})
+export class CrewProfileComponent {
+
+}
